2cadf867739e5e343aa8b6f870eb257728d70008
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-05-14 22:20:37 -0300

    chore: Phone number input structure

--- src/components/Contact/Form/index.tsx
+++ src/components/Contact/Form/index.tsx
@@ -9,6 +9,45 @@ export function Form() {
   const [message, setMessage] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
+  // const phoneFormat = /^\(?([0-9]{2})\)?[-. ]?([0-9]{5})[-. ]?([0-9]{4})$/;
+
+  function handlePhoneInput(inputValue: string) {
+    const inputNumber = inputValue.replace(/[^\d]/g, '');
+    const currentNumberLength = inputNumber.length;
+
+    if (currentNumberLength < 4) {
+      return setPhone(inputNumber);
+    }
+
+    // return: 'x', 'xx'
+    if (currentNumberLength < 6) {
+      const normalizedNumber = `+${inputNumber.slice(0, 2)} ${inputNumber.slice(
+        2
+      )} `;
+      return setPhone(normalizedNumber);
+    }
+
+    // return: '(xx)', '(xx) x', '(xx) xx', '(xx) xxx', '(xx) xxxx'
+    if (currentNumberLength < 9) {
+      const normalizedNumber = `+${inputNumber.slice(
+        0,
+        2
+      )} (${inputNumber.slice(2, 4)}) ${inputNumber.slice(4)} `;
+      return setPhone(normalizedNumber);
+    }
+
+    // return: '(xx) xxxx-', '(xx) xxxx-x', '(xx) xxxx-xx', '(xx) xxxx-xxx', '(xx) xxxx-xxxx'
+    const normalizeNumberCompleted = `+${inputNumber.slice(
+      0,
+      2
+    )} (${inputNumber.slice(2, 4)}) ${inputNumber.slice(
+      4,
+      9
+    )}-${inputNumber.slice(9, 13)}`;
+    return setPhone(normalizeNumberCompleted);
+  }
+  console.log('tipe: ', typeof phone);
+
   async function handleSubmit(e: FormEvent) {
     e.preventDefault();
     setIsLoading(true);
@@ -73,10 +112,10 @@ export function Form() {
         <input
           required
           type='tel'
-          pattern='[0-9]{2}[0-9]{5}[0-9]{4}'
-          placeholder='Ex.: 24992035033'
+          // pattern='([0-9]){2}[0-9]{5}-[0-9]{4}'
+          placeholder='+55 21 9xxxx-xxxx'
           value={phone}
-          onChange={(e) => setPhone(e.target.value)}
+          onChange={(e) => handlePhoneInput(e.target.value)}
         />
         <span>Phone</span>
       </div>
